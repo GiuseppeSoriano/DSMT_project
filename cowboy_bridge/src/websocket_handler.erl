f01dc532b4db388b4b524c1255fdaa844793d7b0
Giuseppe Soriano <102223105+GiuseppeSoriano@users.noreply.github.com>
2024-03-06 18:22:30 +0100

    subscription working

--- cowboy_bridge/src/websocket_handler.erl
+++ cowboy_bridge/src/websocket_handler.erl
@@ -3,57 +3,42 @@
 -export([init/2, websocket_init/1, websocket_handle/2, websocket_info/2, terminate/3]).
 
 init(Req, _Opts) ->
-    erlang:send_after(1000, self(), {send_time, self()}),
-    erlang:send_after(1000, self(), {send_random, self()}),
+    % erlang:send_after(1000, self(), tick), % Avvia un timer centralizzato
     {cowboy_websocket, Req, #{}}.
 
 websocket_init(State) ->
-    % Inizializza l'invio dell'ora attuale e prepara la possibilità di inviare un numero casuale
     State1 = maps:put(subscribe_time, false, State),
     State2 = maps:put(subscribe_random, false, State1),
+    erlang:send_after(1000, self(), tick),
     {ok, State2}.
 
 websocket_handle({text, <<"ping">>}, State) ->
-    %% Gestisci il ping qui, ad esempio inviando un pong o semplicemente ignorandolo.
     {ok, State};
 websocket_handle({ping, Payload}, State) ->
-    %% Rispondi al ping con un pong con lo stesso payload.
     {pong, Payload, State};
 websocket_handle({text, Msg}, State) ->
     case Msg of
         <<"subscribe_time">> ->
             io:format("Subscribed to time updates~n"),
-            erlang:send_after(1000, self(), {send_time, self()}),
             {ok, maps:put(subscribe_time, true, State)};
         <<"subscribe_random">> ->
             io:format("Subscribed to random number updates~n"),
-            erlang:send_after(1000, self(), {send_random, self()}),
             {ok, maps:put(subscribe_random, true, State)}
     end;
 websocket_handle(_, State) -> 
     {ok, State}.
 
-
-websocket_info({send_time, _Ref}, State) ->
-    case maps:get(subscribe_time, State, false) of
-        true ->
-            {Frames, StateUpdated} = send_current_time(State),
-            erlang:send_after(1000, self(), {send_time, self()}),
-            {Frames, StateUpdated};
-        false ->
-            erlang:send_after(1000, self(), {send_time, self()}),
-            {[], State}
-    end;
-websocket_info({send_random, _Ref}, State) ->
-    case maps:get(subscribe_random, State, false) of
-        true ->
-            {Frames, StateUpdated} = send_random_number(State),
-            erlang:send_after(1000, self(), {send_random, self()}),
-            {Frames, StateUpdated};
-        false ->
-            erlang:send_after(1000, self(), {send_random, self()}),
-            {[], State}
-    end;
+websocket_info(tick, State) -> % Gestisci il tick del timer centralizzato
+    {FramesTime, State1} = case maps:get(subscribe_time, State, false) of
+        true -> send_current_time(State);
+        false -> {[], State}
+    end,
+    {FramesRandom, State2} = case maps:get(subscribe_random, State1, false) of
+        true -> send_random_number(State1);
+        false -> {[], State1}
+    end,
+    erlang:send_after(1000, self(), tick), % Riavvia il timer centralizzato
+    {FramesTime ++ FramesRandom, State2}; % Invia tutti i frame accumulati
 websocket_info(_Info, State) ->
     {ok, State}.
 
